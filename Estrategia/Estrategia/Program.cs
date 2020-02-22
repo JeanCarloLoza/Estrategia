1c84002dcafff18e36d03b3f351e23deabd8080f
Jean Carlo Loza Carbajal <jeancarloloza@hotmail.com>
2020-02-22 12:03:58 -0600

    v1 sin patron

--- Estrategia/Estrategia/Program.cs
+++ Estrategia/Estrategia/Program.cs
@@ -6,7 +6,31 @@ namespace Estrategia
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string cNombre = "";
+            string cValor = "";
+            string cFormato = "";
+            Console.WriteLine("Ingresa el nombre de la imagen");
+            cNombre = Console.ReadLine();
+            Console.WriteLine("\nEn que formato deseas guardar la imagen:\n"+
+                "1.-BPN\n"+
+                "2.-PNG\n"+
+                "3.-JPGE\n");
+            cValor = Console.ReadLine();
+
+            switch (cValor) {
+                case "1":
+                    cFormato = "bpn";
+                    break;
+                case "2":
+                    cFormato = "png";
+                    break;
+                case "3":
+                    cFormato = "jpg";
+                    break;
+                default:
+                    throw new Exception("Formato no valido");
+            }
+            Console.WriteLine(string.Format("Se ha guardado la imagen {0}.{1}",cNombre, cFormato));
         }
     }
 }
